ec58fdf2c5b5c893a3dd8fd651a6f2a2ab3637f9
HLNoll <hyuryluis@gmail.com>
2019-08-13 06:25:27 -0300

    introducao

---
+++ basico/helloworld.py
@@ -0,0 +1,2 @@
+# Primeiro Print
+print("Hello World!")
---
+++ basico/mathOperation.py
@@ -0,0 +1,18 @@
+# Opearacao de soma
+print( 2 + 15 )
+
+#Operacao de subtracao
+print( 8 - 5 )
+
+#Operacao de multiplicacao
+print( 4 * 2 )
+
+#Operacao de divisao
+print( 6 / 2 )
+
+#Operacao de Exponenciacao (2 elevado a 3)
+print( 2 ** 3 )
+
+#Operacao de modulo (resolve com resto)
+print(10 % 3)
+
